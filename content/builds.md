46bb9f418555776e874cddb59fa9743cb6af7a7b
Will Pearson <will@mcneel.com>
2018-06-03 10:28:34 +0100

    Add /builds page
    Displays latest builds for SpeckleRhino by pulling data from the
    AppVeyor API.

---
+++ content/builds.md
@@ -0,0 +1,15 @@
++++
+date = "2018-06-03T08:45:40+0100"
+title = "Builds"
+description = "Speckle nightly builds"
+type = "page"
+layout = "about"
++++
+
+Here's one we built earlier.
+
+## [SpeckleRhino](https://github.com/speckleworks/SpeckleRhino)
+
+<div id="listing"><pre>Loading...</pre></div>
+
+<script async defer src="/js/builds.js"></script>
---
+++ themes/speckleworks/static/js/builds.js
@@ -0,0 +1,136 @@
+// based on https://github.com/rufuspollock/s3-bucket-listing
+
+// document.getElementById('listing').innerHTML = "<pre>Loading...</pre>";
+
+var nwo = "speckleworks/SpeckleRhino";
+
+var api_url = "https://ci.appveyor.com/api/";
+
+var url = `${api_url}/projects/${nwo}/history?recordsNumber=10&branch=master`;
+
+var xhr = new XMLHttpRequest();
+xhr.open('get', url, false);
+xhr.send(null);
+
+var json = JSON.parse(xhr.response);
+
+var items = [];
+for (var i = 0; i < json.builds.length; i++) {
+  var build = json.builds[i]
+
+  if (build.status != 'success') {
+    continue;
+  }
+
+  var url = `${api_url}/projects/${nwo}/build/${build.version}`;
+  xhr.open('get', url, false);
+  xhr.send(null);
+
+  var build_json = JSON.parse(xhr.response);
+
+  var jobId = build_json.build.jobs[0].jobId;
+
+
+  var item = {};
+
+  var date = Date.parse(build.finished);
+  var date = new Date(build.finished);
+
+  item.date = date.toISOString();
+  item.number = build.version;
+  item.ci_url = `https://ci.appveyor.com/project/${nwo}/build/${build.version}`;
+
+  item.sha = build.commitId;
+  item.sha_short = item.sha.slice(0,7);
+  item.github_url = `https://github.com/${nwo}/commit/${item.sha}`;
+  // item.message = build.message;
+
+  item.artifact = 'specklerhino.rhi';
+  var artifact_url = `${api_url}/buildJobs/${jobId}/artifacts/specklerhino.rhi`;
+  item.artifact_url = artifact_url;
+
+  item.ref = build.branch;
+  if (build.pullRequestId != null) {
+    item.ref = '#' + build.pullRequestId;
+    item.ref_url = `https://github.com/${nwo}/pull/${build.pullRequestId}`;
+  }
+
+  items.push(item);
+}
+
+document.getElementById('listing').innerHTML = '<pre>' + prepareTable(items) + '</pre>';
+
+function prepareTable(items) {
+  // items is object like:
+  // [
+  //   {
+  //      number: ..
+  //      ref: ..
+  //      sha: ..
+  //      sha_short: ..
+  //      github_url: ..
+  //      artifact: ..
+  //      artifact_url: ..
+  //   },
+  //   ...
+  // ]
+  var cols = [ 29, 10, 12, 25, 16 ];
+  var content = [];
+  content.push(padRight('Date', cols[0]) + padRight('Number', cols[1]) + padRight('SHA', cols[2]) + padRight('Artifact', cols[3]) + 'Branch\n');
+  content.push(new Array(cols[0] + cols[1] + cols[2] + cols[3] + cols[4] + 4).join('-') + '\n');
+
+
+  // jQuery.each(items, function(idx, item) {
+  items.forEach(function(item) {
+    var row = renderRow(item, cols);
+    content.push(row + '\n');
+  });
+
+  return content.join('');
+}
+
+function renderRow(item, cols) {
+  var row = '';
+  row += padRight(item.date, cols[0]);
+  row += padRightUrl(item.number, item.ci_url, cols[1]);
+  row += padRightUrl(item.sha_short, item.github_url, cols[2], item.message);
+  row += padRightUrl(item.artifact, item.artifact_url, cols[3]);
+  if (item.ref_url != null) {
+    row += padRightUrl(item.ref, item.ref_url, cols[4]);
+  } else {
+    row += padRight(item.ref, cols[4]);
+  }
+  // row += '  ';
+  return row;
+}
+
+function padRight(padString, length) {
+  var str = String(padString).slice(0, length-3);
+  if (padString.length > str.length) {
+    str += '...';
+  }
+  while (str.length < length) {
+    str = str + ' ';
+  }
+  return str;
+}
+
+function padRightUrl(padString, padUrl, length, title) {
+  var str = String(padString).slice(0, length-3);
+  if (padString.length > str.length) {
+    str += '...';
+  }
+  var len = str.length;
+  if (title) {
+    title = ' title="' + title + '" ';
+  }
+  else {
+    title = '';
+  }
+  str = '<a href="' + padUrl + '"' + title + '>' + str + '</a>'
+  while (len < length) {
+    str = str + ' ';
+    len++;
+  }
+  return str;
+}
