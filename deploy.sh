5c4d5e48994d33bca6e3c23e3e6c8e7f35e68579
Will Pearson <will@mcneel.com>
2018-06-05 12:44:00 +0100

    Update deploy.sh
    Sets up public/ dir ready for deployment + more logging

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+set -e
+
+echo -e "\033[0;34;4mDeploying updates to GitHub!\033[0m"
+
+echo -e "\n\033[0;32m===> Setting up public/...\033[0m\n"
+
+# Set up, update and clear public/ dir
+rm -rf public/
+git submodule update --init
+cd public
+git checkout master
+git pull
+# rm -rf * # TODO: clean public/ before build
+cd ..
+
+echo -e "\n\033[0;32m===> Building site...\033[0m"
 
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
+echo -e "\n\033[0;32m===> Deploying site...\033[0m\n"
+
 # Go To Public folder
+echo "Entering public/"
 cd public
 # Add changes to git.
 git add .
@@ -21,7 +39,10 @@ git commit -m "$msg"
 git push origin master
 
 # Come Back up to the Project Root
+echo "Exiting public/"
 cd ..
+
+echo -e "\n\033[0;32m===> Updating source...\033[0m\n"
 
 # Add changes to source
 git add .
